4427be42971b36cd50e41d383adf8a778f560147
Arne K <arnekellmann@googlemail.com>
2021-06-22 15:38:38 +0200

    initial commit

---
+++ includes/class-wp-customizer-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Customizer
+ * @subpackage Wp_Customizer/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Customizer
+ * @subpackage Wp_Customizer/includes
+ * @author     Your Name <email@example.com>
+ */
+class Wp_Customizer_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
